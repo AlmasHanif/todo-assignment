757ea82759448ab1b1e397802ad4407c50e0397e
almas998 <almas.haneef@asinlabs.tech>
2023-03-05 02:12:27 +0500

    todo app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [todos, setTodos] = useState([])
+  const [input, setInput] = useState("")
+  const addTodo = () => {
+    if (input !== "") {
+      setTodos([...todos, input])
+      setInput("")
+    }
+  }
+  const deleteTodo = (text) => {
+    const newTodos = todos.filter((todo) => {
+      return todo !== text
+    })
+    setTodos(newTodos)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>
+        Todo App
+      </h1>
+      <input type="text" placeholder='enter your todo' value={input} onChange={(e) => setInput(e.target.value)} />
+      <button onClick={addTodo}>Add Todo</button>
+      <div>
+        {todos.map((val, ind) => (
+          <><div key={ind}>{val}<button onClick={() => deleteTodo(val)}>Delete todo</button></div></>
+        ))}
+      </div>
     </div>
   );
 }
